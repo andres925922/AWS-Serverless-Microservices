ed609958bbdd6902bd6e476ac5a462f0656a37de
Andres Convertini <andres@fdna.com>
2025-05-12 14:09:12 +0200

    move from layer to docker image for lambda fn

--- e-commerce-microservice/e_commerce_microservice/microservices.py
+++ e-commerce-microservice/e_commerce_microservice/microservices.py
@@ -1,6 +1,6 @@
 from abc import ABC
-from aws_cdk import Duration, BundlingOptions, DockerImage
-from aws_cdk.aws_lambda import Function, FunctionAttributes, Runtime, Code, LayerVersion
+from aws_cdk import Duration
+from aws_cdk.aws_lambda import DockerImageFunction, DockerImageCode
 from aws_cdk.aws_dynamodb import  ITable
 
 from constructs import Construct
@@ -25,23 +25,24 @@ class LambdaStack(Construct):
     def __init__(self, scope: Construct, id: str, props: IMicroserviceProps, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
         self._props: IMicroserviceProps = props
-        self._common_layer: LayerVersion = self._create_dependencies_layer()
-        self.products_lambda: Function = self._create_products_lambda_microservice()
-        self.basket_lambda: Function = self._create_basket_lambda_microservice()
-        self.orders_lambda: Function = self._create_orders_lambda_microservice()
+        self.products_lambda: DockerImageFunction = self._create_products_lambda_microservice()
+        self.basket_lambda: DockerImageFunction = self._create_basket_lambda_microservice()
+        self.orders_lambda: DockerImageFunction = self._create_orders_lambda_microservice()
 
-    def _create_products_lambda_microservice(self) -> Function:
-        products_lambda = Function(
+    def _create_products_lambda_microservice(self) -> DockerImageFunction:
+        products_lambda = DockerImageFunction(
             self, "ProductsLambda",
-            runtime=Runtime.PYTHON_3_12,
-            handler="handler.handle",
-            code=Code.from_asset("../src/products"),  # relative to cdk.json
-            layers=[
-                self._common_layer,
-            ], 
+            code=DockerImageCode.from_image_asset(
+                directory="../src",
+                file="Dockerfile",
+                build_args={
+                    "LAMBDA_ROOT_SOURCE": "products"
+                }
+            ),
             environment={
                 "PRIMARY_KEY": "id",
                 "PRODUCTS_TABLE": self._props.products_table.table_name,
+                "LAMBDA_HANDLER": "products.handler.handle",
             },
             memory_size=128,
             timeout=Duration.seconds(30),
@@ -51,18 +52,20 @@ class LambdaStack(Construct):
 
         return products_lambda
     
-    def _create_basket_lambda_microservice(self) -> Function:
-        basket_lambda = Function(
+    def _create_basket_lambda_microservice(self) -> DockerImageFunction:
+        basket_lambda = DockerImageFunction(
             self, "BasketLambda",
-            runtime=Runtime.PYTHON_3_12,
-            handler="handler.handle",
-            code=Code.from_asset("../src/basket"),  # relative to cdk.json
-            layers=[
-                self._common_layer,
-            ], 
+            code=DockerImageCode.from_image_asset(
+                directory="../src",
+                file="Dockerfile",
+                build_args={
+                    "LAMBDA_ROOT_SOURCE": "basket"
+                }
+            ),
             environment={
                 "PRIMARY_KEY": "id",
                 "BASKET_TABLE": self._props.basket_table.table_name,
+                "LAMBDA_HANDLER": "basket.handler.handle",
             },
             memory_size=128,
             timeout=Duration.seconds(30),
@@ -72,40 +75,25 @@ class LambdaStack(Construct):
 
         return basket_lambda
     
-    def _create_orders_lambda_microservice(self) -> Function:
-        orders_lambda = Function(
+    def _create_orders_lambda_microservice(self) -> DockerImageFunction:
+        orders_lambda = DockerImageFunction(
             self, "OrdersLambda",
-            runtime=Runtime.PYTHON_3_12,
-            handler="handler.handle",
-            code=Code.from_asset("../src/orders"),  # relative to cdk.json
-            layers=[
-                self._common_layer,
-            ], 
+            code=DockerImageCode.from_image_asset(
+                directory="../src",
+                file="Dockerfile",
+                build_args={
+                    "LAMBDA_ROOT_SOURCE": "orders"
+                }
+            ),
             environment={
                 "PRIMARY_KEY": "id",
                 "ORDERS_TABLE": self._props.orders_table.table_name,
+                "LAMBDA_HANDLER": "orders.handler.handle",
             },
             memory_size=128,
             timeout=Duration.seconds(30),
         )
 
-        self._props.basket_table.grant_read_write_data(orders_lambda)
+        self._props.orders_table.grant_read_write_data(orders_lambda)
 
         return orders_lambda
-
-    def _create_dependencies_layer(self) -> LayerVersion:
-        return LayerVersion(
-            self, "DependenciesLayer",
-            code=Code.from_asset(
-                path="../src/_layers/dependencies",  # where your requirements.txt is
-                bundling=BundlingOptions(
-                    image=DockerImage.from_registry("python:3.12-slim"), 
-                    command=[
-                        "bash", "-c",
-                        "pip install --no-cache-dir -r requirements.txt -t /asset-output/python && cp -r python/* /asset-output/python/ "
-                    ],
-                ),
-            ),
-            compatible_runtimes=[Runtime.PYTHON_3_12],
-            description="Python libraries like boto3, requests, etc."
-        )
